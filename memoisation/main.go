216580992a767adbd08587d87435113047b5d091
tanima.halder_xeb <tanima.halder_xeb@idfcbank.com>
2022-05-04 10:09:03 +0530

    adding memoisation

---
+++ memoisation/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	cache := make(map[int64]int64)
+	for i := int64(1); i < 100; i++ {
+		if i == 93 {
+			fmt.Printf("%d + %d = %d ??? weird, what happened?)\n", cache[92], cache[91], cache[92]+cache[91])
+		}
+		fmt.Println(fib(i, cache))
+	}
+}
+
+func fib(n int64, cache map[int64]int64) int64 {
+	if n == 1 || n == 2 {
+		return 1
+	}
+	if cache[n] == 0 {
+		cache[n] = fib(n-1, cache) + fib(n-2, cache)
+	}
+	return cache[n]
+}
